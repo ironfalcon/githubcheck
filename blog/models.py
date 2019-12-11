76e805c0c95f1bba709c271b6a5f6294a433c07c
Sergey Biryukov <sbiryukov@griddynamics.com>
2019-12-11 20:57:33 +0400

    update test

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,12 @@ class Post (models.Model):
 
     def __str__(self):
         return self.title
+
+class Report(models.Model):
+    ldap_login = models.CharField(max_length=50, blank=True, unique=True)
+    github_login = models.CharField(max_length=100, blank=True)
+    checked = models.CharField(default='NONE', max_length=6) 
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.ldap_login
--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .models import Post
+from .models import Post, Report
+
 
 def create_post(request):
     posts = Post.objects.all()
     response_data = {}
 
     if request.POST.get('action') == 'post':
-        title = request.POST.get('title')
-        description = request.POST.get('description')
+        try:
+            entity = Report.objects.get(github_login=request.POST.get('github_login'))
+        except (KeyError, Report.DoesNotExist):
+            # Redisplay the question voting form.
+            github_login = request.POST['github_login']
+            response_data['github'] = github_login
+            return JsonResponse(response_data)
+        else:
+            ldap = entity.ldap_login
+            github = entity.github_login
+            response_data['ldap'] = ldap
+            response_data['github'] = github
+            return JsonResponse(response_data)
 
-        response_data['title'] = title
-        response_data['description'] = description
-      
-        Post.objects.create(
-            title = title,
-            description = description,
-            )
-        return JsonResponse(response_data)
 
-    return render(request, 'create_post.html', {'posts':posts})    
+
+    return render(request, 'main.html')    
